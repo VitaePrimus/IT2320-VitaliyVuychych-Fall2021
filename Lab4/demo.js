b33074b6dff780c3234d4981e539a8eae761833d
Vitaliy Vuychych <vitaliy202@gmail.com>
2021-09-25 19:31:18 -0400

    finished commit

--- Lab4/demo.js
+++ Lab4/demo.js
@@ -18,7 +18,7 @@ function CalculateCommission()
     // 3. Save January sales amount in an array
     const january = [10000, 25000, 20000, 14000, 30000, 16000, 30000];
     // 4. Save February sales amount in an array
-    const february = [8000, 7500, 45000, 10000, 38000, 22000, 15000]
+    const february = [8000, 7500, 45000, 10000, 38000, 22000, 15000];
 
     const totalSales = [];
     const commission = [];
@@ -52,17 +52,17 @@ function CalculateCommission()
     // Calculate average commission:
     for(var i = 0; i < commission.length; i++){
         avgComm[i] = commission[i] / 3;
-        avgComm[i] = Math.round(avgComm[i] * 100) / 100     // Made this to round up avg commission to 2 decimals after the point
+        avgComm[i] = Math.round(avgComm[i] * 100) / 100;     // Made this to round up avg commission to 2 decimals after the point
     }
     // Display sales person name, commission and average commission
-    console.log("Name\t\t" + "Commission\t\t" + "Average Commission")
-    console.log("-------------------------------------------------")
+    console.log("Name\t\t" + "Commission\t\t" + "Average Commission");
+    console.log("-------------------------------------------------");
 
     for(var i = 0; i < commission.length; i++){
         console.log(name[i] + "\t\t " + commission[i] + "\t\t\t" + avgComm[i]);
     }
 
-    console.log("-------------------------------------------------")
+    console.log("-------------------------------------------------");
 
     // Store the commission to a "topCommission" variable.
     for(var i = 0; i < commission.length; i++){
@@ -73,9 +73,9 @@ function CalculateCommission()
     }
 
     // 6. Outside the "For" loop, display topCommission and topSalesperson name.
-    alert("The top sales person is " + topSalesPerson + ", with the top comission of " + topCommission + ".")
-    console.log("____________________________________________________________________")
-    console.log("The top sales person is " + topSalesPerson + ", with the top comission of " + topCommission + ".")
-    console.log("____________________________________________________________________")
+    alert("The top salesperson is " + topSalesPerson + ", with the top comission of " + topCommission + ".");
+    console.log("____________________________________________________________________");
+    console.log("The top salesperson is " + topSalesPerson + ", with the top comission of " + topCommission + ".");
+    console.log("____________________________________________________________________");
     
 }
