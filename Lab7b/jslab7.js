bc9a884d2d78a838f80fcbec4ed132096c51504f
Vitaliy Vuychych <vitaliy202@gmail.com>
2021-10-18 19:08:15 -0400

    commit lab7b

--- Lab7b/jslab7.js
+++ Lab7b/jslab7.js
@@ -1,7 +1,29 @@
 /*********************************************************************/
 //Global Area
+window.onload = function(){
+    var btns = document.getElementsByClassName("btnOrig");
 
+    for(var i = 0; i < btns.length; i++){
+        btns[i].addEventListener("click", function(){
+            ChangeButtonText(this);
+        });
+    }
 
+    var sqrs = document.getElementsByClassName("square3");
+
+    for(var i = 0; i < sqrs.length; i++){
+        sqrs[i].addEventListener("click", function(){
+            ChangeSquare(this);
+        });
+    }
+}
 
 
 /***************** Function definition area *************/
+function ChangeButtonText(btns){
+    btns.innerText = "Active";
+}
+
+function ChangeSquare(sqrs){
+    sqrs.style.backgroundColor = "green";
+}
